7dc73b30919ef223f10a53b09ca2dcf7088bb270
ggszk <gengo.suzuki@gmail.com>
2025-07-05 12:27:57 +0900

    fix: audio_lib/__init__.pyにTriangleWaveとSequencerのインポートを追加
    - ImportError: cannot import name 'TriangleWave' を解消
    - TriangleWaveクラスはoscillators.pyに存在するが__init__.pyで公開されていなかった
    - Sequencerクラスも同様に追加してノートブックで使用可能にした
    - lesson_04および他のノートブックで必要なクラス・関数が正常にインポート可能

--- audio_lib/__init__.py
+++ audio_lib/__init__.py
@@ -6,11 +6,12 @@
 
 from .core.audio_config import AudioConfig
 from .core.wave_io import WaveFileIO
-from .synthesis.oscillators import SineWave, SawtoothWave, SquareWave
+from .synthesis.oscillators import SineWave, SawtoothWave, SquareWave, TriangleWave
 from .synthesis.envelopes import ADSREnvelope, LinearEnvelope
 from .effects.filters import LowPassFilter, HighPassFilter
 from .effects.audio_effects import Reverb, Distortion
 from .synthesis.note_utils import note_to_frequency, frequency_to_note, note_name_to_number
+from .sequencer import Sequencer
 
 __version__ = "1.0.0"
 __author__ = "音のプログラミング教育チーム"
